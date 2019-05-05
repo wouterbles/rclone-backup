2922b5486367d1cb87acb4ea9eda8ac0fdc6d124
Wouter Bles <whbles@gmail.com>
2019-05-05 15:37:13 +0200

    Read bucket name from .env variable

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+source .env
 now=`date +%Y-%m-%d.%H:%M:%S`
 zip -qr $now.zip /var/www
-rclone copy $now.zip b2:rclone-backup-[unique name]
+rclone copy $now.zip b2:$B2_BUCKET_NAME
 rm $now.zip
